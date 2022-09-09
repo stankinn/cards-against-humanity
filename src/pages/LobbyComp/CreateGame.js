e8d0bd6e5767d2e7788d60340e69728feeada249
stvibuzo <vivienne.s.buzov@student.hs-anhalt.de>
2022-09-09 12:08:44 +0200

    IDK was ich commiten soll

---
+++ src/pages/LobbyComp/CreateGame.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { serviceendpoint } from '../Imports';
+import { lang } from '../../Languages';
+
+export default function CreateGame(props){
+    
+}
