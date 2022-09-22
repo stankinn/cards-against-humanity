be136eee8991fd6542cc09f16c270a0ff0ffa195
stankinn <anne.kinne@student.hs-anhalt.de>
2022-09-22 14:40:49 +0200

    ingamePacksUpdate in useEffect

--- src/pages/GameComp/IngamePacks.js
+++ src/pages/GameComp/IngamePacks.js
@@ -14,7 +14,7 @@ export default function PlayerPoints(props){
 
     useEffect(() =>{
         updatePacks();
-    });
+    }, []);
 
     function updatePacks(){
         fetch(serviceendpoint + '/games/')
