e6ea6b8415b9ad0fb44228c87daf52b9bb7f8694
stkkkrau <katharina.kraus@student.hs-anhalt.de>
2022-09-24 15:45:56 +0200

    kleinigkeiten

--- src/pages/GameComp/BlackCard.js
+++ src/pages/GameComp/BlackCard.js
@@ -17,44 +17,46 @@ export default function BlackCard() {
 
     useEffect(() => {
         const interval = setInterval(() => {
-        fetch(serviceendpoint + '/games/')
-            .then(response => response.json())
-            .then(data => {
-                for (var i = 0; i < data.games.length; i++) {
-                    //check if selected game is running: if so, set prompt and running state
-                    if (data.games[i].id === Number(localStorage.getItem('gameID'))) {
-                        if (data.games[i].running === true) {
-                            fetch(serviceendpoint + '/games/' + Number(localStorage.getItem('gameID')))
-                                .then(response => response.json())
-                                .then(data => {
-                                    str = data.currentBlackCard.text;
-                                    cardText = str.replaceAll('_', ' _______ ');
-                                    setPromt(cardText);
-                                    setRunning(true);
-                                })
-                                .catch((error) => {
-                                    console.error('Error:', error);
-                                });
-                        } else {
+            fetch(serviceendpoint + '/games/')
+                .then(response => response.json())
+                .then(data => {
+                    for (var i = 0; i < data.games.length; i++) {
+                        //check if selected game is running: if so, set prompt and running state
+                        if (data.games[i].id === Number(localStorage.getItem('gameID'))) {
+                            if (data.games[i].running === true) {
+                                fetch(serviceendpoint + '/games/' + Number(localStorage.getItem('gameID')))
+                                    .then(response => response.json())
+                                    .then(data => {
+                                        str = data.currentBlackCard.text;
+                                        cardText = str.replaceAll('_', ' _______ ');
+                                        setPromt(cardText);
+                                        setRunning(true);
+                                    })
+                                    .catch((error) => {
+                                        console.error('Error:', error);
+                                    });
+                            } else {
 
-                            //if game is not running, return to gamelobby
-                            setRunning(false);
-                            navigate('/lobby/' + Number(localStorage.getItem('gameID')));
+                                //if game is not running, return to gamelobby
+                                setRunning(false);
+
+                                navigate('/lobby/' + Number(localStorage.getItem('gameID')));
+
+                            }
+                            i = data.games.length;
+                            break;
                         }
-                        i = data.games.length;
-                        break;
                     }
-                }
 
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, 500);
-    return () => clearInterval(interval);
-    }, [])
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }, 500);
+        return () => clearInterval(interval);
+    })
 
-//display of text
+    //display of text
     if ({ running }.running === true) {
         return (
             <>
